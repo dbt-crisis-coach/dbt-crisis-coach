72b89d9e7596bf50bef4330360a45dd1b569e09e
Michael Smith <michael@msmith.nz>
2015-10-22 21:48:34 +1300

    Add back button to profile

--- www/js/controllers/communcationController.js
+++ www/js/controllers/communcationController.js
@@ -1,8 +1,14 @@
 angular.module('dbt')
-.controller('CommunicationCtr',function($stateParams, ContactService, $scope) {	
+.controller('CommunicationCtr',function($stateParams, ContactService, $ionicHistory) {	
 	var self = this;
+	
+	self.goBack  = goBack;
 	
 	ContactService.contact($stateParams.contactId).then(function(contact) {
 					self.title = contact.name;
 				});
+				
+	function goBack() {
+		$ionicHistory.goBack();
+	}
 });
