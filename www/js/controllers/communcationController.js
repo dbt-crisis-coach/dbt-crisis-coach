cbc8a4c0c966f9cd33b8e867fb95b385c0e90028
Michael Smith <michael@msmith.nz>
2015-10-30 00:42:27 +1300

    Add read SMS ability fro Communications screen

--- www/js/controllers/communcationController.js
+++ www/js/controllers/communcationController.js
@@ -1,16 +1,22 @@
 angular.module('dbt')
-.controller('CommunicationCtr',function($stateParams, ContactService, $state, $ionicViewSwitcher) {	
+.controller('CommunicationCtr',function($stateParams, ContactService, $state, $ionicViewSwitcher, CommunicationsService) {	
 	var self = this;
 	
 	self.goBack  = goBack;
+	self.texts = [];
 	
 	ContactService.contact($stateParams.contactId).then(function(contact) {
 		self.title = contact.name;
+		return CommunicationsService.readTexts(contact.number);
+	})
+	.then(function(texts) {
+		//stop loading ticker
+		self.texts = texts;
 	})
 	.catch(function(error) {
 		console.log(error.message);
 		alert('Cannot find that contact');
-	});;
+	});
 				
 	function goBack() {
 		$ionicViewSwitcher.nextDirection('back');
--- www/js/services/communicationsService.js
+++ www/js/services/communicationsService.js
@@ -1,4 +1,27 @@
 angular.module('dbt')
-.factory('CommunicationsService', function() {
+.factory('CommunicationsService', function($ionicPlatform, $q) {
+	
+	var service = {
+		readTexts: readTexts
+	}
+	return service;
+	
+	function readTexts(contactNumber) {
+		
+		var filter = {
+                box : '',
+                address : contactNumber.replace(/[^\d\+]/g,"")
+            }
+			
+		return $ionicPlatform.ready().then(function() {
+			return $q(function(resolve, reject) {
+				SMS.listSMS(filter, function(data) {
+					resolve(data);
+				}, function(error) {
+					reject(error);
+				});
+			});
+		});
+	}
 	
 });
