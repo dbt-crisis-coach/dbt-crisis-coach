c27cb624f8c0124e7af2dca8b233d57c40085a1e
Mr-Campbell-Hawkes <mr.campbell.hawkes@gmail.com>
2015-12-10 19:20:02 +1300

    Patient Profile Renders to Page

--- www/js/controllers/informationController.js
+++ www/js/controllers/informationController.js
@@ -6,6 +6,13 @@ angular.module('dbt')
 	
 	ContactService.contact($stateParams.contactId).then(function(contact) {
 		self.title = contact.name;
+		self.number = contact.number;
+		// Mock Details
+		self.details = {
+			pref: "Asking Michael about it",
+			triggers: ["Misunderstanding Angular/Ionic as a combo", "knowing it's nap time but I'm so..close..."],
+			extra: "It might be time for bed"
+		}
 	})
 	.catch(function(error) {
 		console.log(error.message);
