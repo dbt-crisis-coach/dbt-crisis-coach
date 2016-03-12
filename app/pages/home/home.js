aa1cb5d0ca4193f398af2250a6772122ab99e836
Michael Smith <michael@msmith.nz>
2016-03-12 22:45:46 +1300

    Remove cucumber

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,15 +1,19 @@
-import {Page} from 'ionic-angular';
+import {Page} from 'ionic-angular'
 import {Contacts} from 'ionic-native'
 
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
+
 export class HomePage {
+  constructor() {
+    this.numbers = []
+  }
   findContact() {
     return Contacts.pickContact().then((contact) => {
-      contact.phoneNumbers.map((number) => {
-        console.log(number.value)
+      this.numbers = contact.phoneNumbers.map((number) => {
+        return number.value
       })
     })
   }
