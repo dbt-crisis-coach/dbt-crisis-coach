dbeeec92ae470e0db348fc8f761bbc664157a6a1
Michael Smith <michael@msmith.nz>
2016-03-12 16:12:12 +1300

    Add find contact from native contact app

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,7 +1,16 @@
 import {Page} from 'ionic-angular';
+import {Contacts} from 'ionic-native'
 
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {}
+export class HomePage {
+  findContact() {
+    return Contacts.pickContact().then((contact) => {
+      contact.phoneNumbers.map((number) => {
+        console.log(number.value)
+      })
+    })
+  }
+}
