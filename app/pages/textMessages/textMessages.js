b57b9ba828cb8644a66c582c1597776e6442e8a7
Michael Smith <michael@msmith.nz>
2016-06-13 15:24:01 +1200

    add sms to csv support

--- app/pages/textMessages/textMessages.js
+++ app/pages/textMessages/textMessages.js
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core'
 import {NavParams, Loading, NavController} from 'ionic-angular'
 import {SMS} from '../providers/sms'
+import json2csv from 'json2csv'
 
 
 @Component({
@@ -21,5 +22,19 @@ export class TextMessagesPage {
     sms.list(this.params.number).then((data) => {
       this.params.sms = data
     })
+  }
+
+  report() {
+    let fields = ['date', 'type', 'body']
+    json2csv({ data: this.params.sms, fields: fields }, function(err, csv) {
+      if (err) console.log(err);
+       console.log(csv);
+    });
+  }
+
+  convertToCSV() {
+    //convert date to readable format
+    //Change type to sender or receiver
+    
   }
 }
