e9f067c10f20c76ea2cbf487801b7d8546ac629f
Michael Smith <michael@msmith.nz>
2016-06-13 14:47:56 +1200

    Add example PDF generation

--- app/pages/textMessages/textMessages.js
+++ app/pages/textMessages/textMessages.js
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core'
 import {NavParams, Loading, NavController} from 'ionic-angular'
 import {SMS} from '../providers/sms'
+import jsPDF from 'jspdf-yworks'
 
 
 @Component({
@@ -21,5 +22,17 @@ export class TextMessagesPage {
     sms.list(this.params.number).then((data) => {
       this.params.sms = data
     })
+  }
+
+  report() {
+   var doc = new jsPDF()
+  doc.text(20, 20, 'Hello world!')
+  doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.')
+  doc.addPage()
+  doc.text(20, 20, 'Do you like that?')
+
+// Output as Data URI
+
+      console.log(doc.output('datauri'))
   }
 }
